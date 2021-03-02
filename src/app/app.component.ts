97af5709a4dc9e6666f5fa824757d9f2f87a9a84
Prasanna Venkatesh Kumar RTV <pvk13797@gmail.com>
2021-03-02 16:45:45 +0530

    json-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Json-angular';
+
+  title = 'Retrieve JSON Data Using Angular';
+  url = 'https://jsonplaceholder.typicode.com/posts';
+  items = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).subscribe((data) => {
+      console.log(data);
+
+      for (let key in data)
+        if (data.hasOwnProperty(key))
+          this.items.push(data[key]);
+    });
+  }
+
+  deleteRow(id) {
+      for(let i = 0; i < this.items.length; i++) {
+        if (this.items[i].id === id) {
+          this.items.splice(i,1);
+        }
+      }
+  }
 }
